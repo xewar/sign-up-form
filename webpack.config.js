05c37a3a31bd818c2de89d56fb50d0bff5db254d
Xewar <xewardreams@gmail.com>
2022-02-11 14:59:43 -0500

    added height and width to svg

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
                 use: ['style-loader','css-loader'],
             },
             {
-                test: /\.(png|svg|jpg|JPG|PNG|jpeg|gif)$/i,
+                test: /\.(png|svg|SVG|jpg|JPG|PNG|jpeg|gif)$/i,
                 type: 'asset/resource',
             },
             {
